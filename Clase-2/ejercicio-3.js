c1abe19c909d5d02e993073407063487f15c654f
MatiasDuro-ORT <matias.duro@ort.edu.ar>
2024-08-20 22:01:55 -0300

    subir ejercicio 3

---
+++ Clase-2/ejercicio-3.js
@@ -0,0 +1,28 @@
+
+// separarPorPalabrasPares
+
+function separarPorPares(){
+
+
+
+    for (let i = 0; i < array.length; i++) {
+        
+    }
+
+    return resultado
+}
+
+
+let palabras = ['Auto', 'Parlante', 'Moto', 'Sal', 'Perfume', 'estrella', 'Sol', 'Nubes']
+
+const resultado = separarPorPares()
+
+console.log(resultado);
+
+
+// Estrucutra del resultado
+
+// resultado = {
+//     pares:[...],
+//     impares:[...]:
+// }
