290c44099b65c0dcdbc2462860dfc8728bb82dd3
MatiasDuro-ORT <matias.duro@ort.edu.ar>
2024-09-03 21:21:38 -0300

    subir clase 4

---
+++ Clase-4/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './App.css'
+import { Principal } from './components/Principal/Principal'
+import { Contador } from './components/Contador/Contador'
+
+function App() {
+
+  return (
+    <>
+    {/* <Principal /> */}
+    <Contador />
+    </>
+  )
+}
+
+export default App
---
+++ Clase-4/src/components/Contador/Contador.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+export const Contador = () => {
+
+// Inicialicen el estado del contador
+
+// Funcion de Incrementar en 1
+
+// Funcion de Decrementar en 1, debe parar si el valor es 0
+
+
+// Return debe devolver
+
+// 2 Botones, uno que diga Incrementar Los Alumnos en +1 y otro que diga
+// Reducir los alumnos en -1
+// Devolver el componente Titulo, que diga Contador: mediante prop
+
+
+  return (
+    <h2>Contador de Alumnos: </h2>
+  )
+}
